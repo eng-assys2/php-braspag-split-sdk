b3c5807e386b1ee18666c21f0a2facdf10ca1a10
Lucas Assis <lukas.ecomp@gmail.com>
2019-06-25 12:03:38 -0300

    Add documentation for FraudAnalysisItems attributes

--- src/Cielo/API30/Ecommerce/FraudAnalysis/FraudAnalysisItems.php
+++ src/Cielo/API30/Ecommerce/FraudAnalysis/FraudAnalysisItems.php
@@ -10,39 +10,117 @@ namespace Cielo\API30\Ecommerce\FraudAnalysis;
 class FraudAnalysisItems implements CieloSerializable
 {
 
+    /** @var string|null Campo que avaliará os endereços de cobrança e entrega para diferentes cidades, estados ou países.
+     * “Yes”(Em caso de divergência entre endereços de cobrança e entrega, marca como risco pequeno)
+     * “No”(Em caso de divergência entre endereços de cobrança e entrega, marca com risco alto)
+     * “Off”(Ignora a análise de risco para endereços divergentes)
+     * */
     private $giftCategory;
+
+    /** @var string|null Nível de importância do e-mail e endereços IP dos clientes em risco de pontuação.
+     * “Low”(Baixa importância do e-mail e endereço IP na análise de risco)
+     * “Normal”(Média importância do e-mail e endereço IP na análise de risco)
+     * “High”(Alta importância do e-mail e endereço IP na análise de risco)
+     * “Off”(E-mail e endereço IP não afetam a análise de risco) 
+     * */
     private $hostHedge;
+
+    /** @var string|null Nível dos testes realizados sobre os dados do comprador com pedidos recebidos sem sentido.
+     * “Low”(Baixa importância da verificação feita sobre o pedido do comprador, na análise de risco)
+     * “Normal”(Média importância da verificação feita sobre o pedido do comprador, na análise de risco)
+     * “High”(Alta importância da verificação feita sobre o pedido do comprador, na análise de risco)
+     * “Off”(Verificação do pedido do comprador não afeta a análise de risco) 
+     * */
     private $nonSensicalHedge;
+
+    /** @var string|null Nível de obscenidade dos pedidos recebedidos.
+     * “Low”(Baixa importância da verificação sobre obscenidades do pedido do comprador, na análise de risco)
+     * “Normal”(Média importância da verificação sobre obscenidades do pedido do comprador, na análise de risco)
+     * “High”(Alta importância da verificação sobre obscenidades do pedido do comprador, na análise de risco)
+     * “Off”(Verificação de obscenidade no pedido do comprador não afeta a análise de risco) 
+     * */
     private $obscenitiesHedge;
+
+    /** @var string|null Nível dos testes realizados com os números de telefones.
+     * “Low”(Baixa importância nos testes realizados com números de telefone)
+     * “Normal”(Média importância nos testes realizados com números de telefone)
+     * “High”(Alta importância nos testes realizados com números de telefone)
+     * “Off”(Testes de números de telefone não afetam a análise de risco) 
+     * */
     private $phoneHedge;
+
+    /** @var string Nome do Produto. */
     private $name;
+
+    /** @var integer Quantidade do produto a ser adquirido. */
     private $quantity;
+
+    /** @var string Código comerciante identificador do produto. */
     private $sku;
+
+    /** @var integer Preço unitário do produto. */
     private $unitPrice;
+
+    /** @var string|null Nível do risco do produto.
+     * Low (O produto tem um histórico de poucos chargebacks)
+     * Normal (O produto tem um histórico de chargebacks considerado normal)
+     * High (O produto tem um histórico de chargebacks acima da média) 
+     * */
     private $risk;
+
+    /** @var string|null Nível de importância da hora do dia do pedido do cliente.
+     * Low (Baixa importância no horário do dia em que foi feita a compra, para a análise de risco)
+     * Normal (Média importância no horário do dia em que foi feita a compra, para a análise de risco)
+     * High (Alta importância no horário do dia em que foi feita a compra, para a análise de risco)
+     * Off (O horário da compra não afeta a análise de risco) 
+     * */
     private $timeHedge;
+
+    /** @var string|null Tipo do produto.
+     * AdultContent(Conteúdo adulto)
+     * Coupon(Cupon de desconto)
+     * Default(Opção padrão para análise na CyberSource quando nenhum outro valor é selecionado)
+     * EletronicGood(Produto eletrônico)
+     * EletronicSoftware(Softwares distribuídos eletronicamente via download)
+     * GiftCertificate(Vale presente)
+     * HandlingOnly(Taxa de instalação ou manuseio)
+     * Service(Serviço)
+     * ShippingAndHandling(Frete e taxa de instalação ou manuseio)
+     * ShippingOnly(Frete)
+     * Subscription(Assinatura) 
+     * */
     private $type;
+
+    /** @var string|null Nível de importância de frequência de compra do cliente.
+     * Low (Baixa importância no número de compras realizadas pelo cliente nos últimos 15 minutos)
+     * Normal (Média importância no número de compras realizadas pelo cliente nos últimos 15 minutos)
+     * High (Alta importância no número de compras realizadas pelo cliente nos últimos 15 minutos)
+     * Off (A frequência de compras realizadas pelo cliente não afeta a análise de fraude) 
+     * */
     private $velocityHedge;
+
+    /** @var FraudAnalysisPassenger|null */
     private $passenger;
+
 
     /**
      * FraudAnalysisItems constructor.
      *
      */
-    public function __construct($giftCategory,
-                                $hostHedge,
-                                $nonSensicalHedge,
-                                $obscenitiesHedge,
-                                $phoneHedge,
-                                $name,
+    public function __construct($name,
                                 $quantity,
                                 $sku,
                                 $unitPrice,
-                                $risk,
-                                $timeHedge,
-                                $type,
-                                $velocityHedge,
-                                $passenger)
+                                $giftCategory=null,
+                                $hostHedge=null,
+                                $nonSensicalHedge=null,
+                                $obscenitiesHedge=null,
+                                $phoneHedge=null,
+                                $risk=null,
+                                $timeHedge=null,
+                                $type=null,
+                                $velocityHedge=null,
+                                $passenger=null)
     {
         $this->giftCategory = $giftCategory;
         $this->hostHedge = $hostHedge;
