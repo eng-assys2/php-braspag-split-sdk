fcc01a4decb03a34984bf6d20086ad81472a6da9
Lucas Assis <lukas.ecomp@gmail.com>
2019-06-25 12:20:33 -0300

    Add attribute browserFingerprint to FraudAnalysisShipping

--- src/Cielo/API30/Ecommerce/FraudAnalysis/FraudAnalysisShipping.php
+++ src/Cielo/API30/Ecommerce/FraudAnalysis/FraudAnalysisShipping.php
@@ -9,16 +9,31 @@ namespace Cielo\API30\Ecommerce\FraudAnalysis;
  */
 class FraudAnalysisShipping implements CieloSerializable
 {
-
+    /** @var string|null Nome do destinatário da entrega. */
     private $addressee;
+
+    /** @var string|null Tipo de serviço de entrega do produto.
+     * “SameDay” (Serviço de entrega no mesmo dia)
+     * “OneDay” (Serviço de entrega noturna ou no dia seguinte)
+     * “TwoDay” (Serviço de entrega em dois dias)
+     * “ThreeDay” (Serviço de entrega em três dias)
+     * “LowCost” (Serviço de entrega de baixo custo)
+     * “Pickup” (Produto retirado na loja)
+     * “Other” (Outro método de entrega)
+     * “None” (Sem serviço de entrega, pois é um serviço ou assinatura)
+     * */
     private $method;
+
+    /** @var string|null Telefone do destinatário da entrega. Ex. 552133665599 (Código do Pais 55, Código da Cidade 21, Telefone 33665599) */
     private $phone;
 
     /**
      * FraudAnalysisShipping constructor.
      *
      */
-    public function __construct($addressee, $method, $phone)
+    public function __construct($addressee=null,
+                                $method=null,
+                                $phone=null)
     {
         $this->addressee = $addressee;
         $this->method = $method;
